9e00a4965c85f17ff65a93938234043bca1733a7
him123 <himanshu.dhakecha@gmail.com>
2022-05-02 18:26:41 +0530

    login design - 4

--- lib/src/login_screen.dart
+++ lib/src/login_screen.dart
@@ -20,70 +20,65 @@ class _MFELoginScreenState extends State<MFELoginScreen> {
       appBar: AppBar(
         title: const Text('MFE Login Screen'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            autofocus: false,
-            controller: textEditingControllerEmail,
-            keyboardType: TextInputType.text,
-            enabled: false,
-            textInputAction: TextInputAction.next,
-            decoration: InputDecoration(
-              focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFF61AE21))),
-              contentPadding: const EdgeInsets.only(left: 20.0),
-              border: const OutlineInputBorder(),
-              errorStyle: const TextStyle(height: 0, fontSize: 0),
-              hintText: 'Email',
-              labelText: 'Email',
-              suffixIcon: Icon(
-                Icons.keyboard_arrow_down,
-                color: UtilIutilsmport().colorUtils.colorGrayIcon,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              TextField(
+                autofocus: false,
+                controller: textEditingControllerEmail,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF61AE21))),
+                  contentPadding: const EdgeInsets.only(left: 20.0),
+                  border: const OutlineInputBorder(),
+                  errorStyle: const TextStyle(height: 0, fontSize: 0),
+                  hintText: 'Email',
+                  labelText: 'Email',
+                  labelStyle:
+                      hintTextStyle.copyWith(color: const Color(0xFF7D858C)),
+                  hintStyle: const TextStyle(
+                    color: Color(0xFF7D858C),
+                  ),
+                ),
               ),
-              labelStyle:
-                  hintTextStyle.copyWith(color: const Color(0xFF7D858C)),
-              hintStyle: const TextStyle(
-                color: Color(0xFF7D858C),
+              WidgetUtil().verticalSpace(10),
+              TextField(
+                autofocus: false,
+                controller: textEditingControllerPassword,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF61AE21))),
+                  contentPadding: const EdgeInsets.only(left: 20.0),
+                  border: const OutlineInputBorder(),
+                  errorStyle: const TextStyle(height: 0, fontSize: 0),
+                  hintText: 'Password',
+                  labelText: 'Password',
+                  labelStyle:
+                      hintTextStyle.copyWith(color: const Color(0xFF7D858C)),
+                  hintStyle: const TextStyle(
+                    color: Color(0xFF7D858C),
+                  ),
+                ),
               ),
-            ),
+              WidgetUtil().verticalSpace(30),
+              Center(
+                child: ElevatedButton(
+                  child: const Text('Login'),
+                  onPressed: () {
+                    // Navigate to second route when tapped.
+                    Navigator.pop(context, '123');
+                  },
+                ),
+              ),
+            ],
           ),
-          WidgetUtil().verticalSpace(10),
-          TextField(
-            autofocus: false,
-            controller: textEditingControllerPassword,
-            keyboardType: TextInputType.text,
-            enabled: false,
-            textInputAction: TextInputAction.next,
-            decoration: InputDecoration(
-              focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFF61AE21))),
-              contentPadding: const EdgeInsets.only(left: 20.0),
-              border: const OutlineInputBorder(),
-              errorStyle: const TextStyle(height: 0, fontSize: 0),
-              hintText: 'Password',
-              labelText: 'Password',
-              suffixIcon: Icon(
-                Icons.keyboard_arrow_down,
-                color: UtilIutilsmport().colorUtils.colorGrayIcon,
-              ),
-              labelStyle:
-                  hintTextStyle.copyWith(color: const Color(0xFF7D858C)),
-              hintStyle: const TextStyle(
-                color: Color(0xFF7D858C),
-              ),
-            ),
-          ),
-          WidgetUtil().verticalSpace(30),
-          Center(
-            child: ElevatedButton(
-              child: const Text('Back to the app with data'),
-              onPressed: () {
-                // Navigate to second route when tapped.
-                Navigator.pop(context, 'xxx');
-              },
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
